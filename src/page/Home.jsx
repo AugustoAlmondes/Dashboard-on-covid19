c21b72d6bb8ac73a8b315161633e3e8f47e32b7d
AugustoAlmondes <augusto7666@gmail.com>
2025-05-27 01:39:25 -0300

    Add Animation on Home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,43 +1,101 @@
 // import './Home.module.css';
 import { FaArrowDownLong } from 'react-icons/fa6';
 import styles from './Home.module.css';
+import { motion as MOTION } from 'motion/react';
 
 export default function Home() {
     return (
         <>
             <div className={styles.home_container}>
                 <div className={styles.overlay} />
-                <video src="https://static.vecteezy.com/system/resources/previews/007/525/680/animation-of-beautiful-red-light-radial-background-free-video.webm"
-                    autoPlay loop muted></video>
+                <MOTION.video
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 4, delay: 1 }}
+
+                    src="https://static.vecteezy.com/system/resources/previews/007/525/680/animation-of-beautiful-red-light-radial-background-free-video.webm"
+
+                    autoPlay loop muted
+
+                />
                 <header className={styles.header}>
-                    <h2>Dash</h2>
+                    <MOTION.h2
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 3, delay: 2.4 }}
+                        className={styles.logo}
+                    >Dash</MOTION.h2>
                     <ul>
-                        <li>
+                        <MOTION.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 3, delay: 2.5 }}
+                        >
                             Home
-                        </li>
-                        <li>
+                        </MOTION.li>
+                        <MOTION.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 3, delay: 2.6 }}
+                        >
                             Sobre
-                        </li>
-                        <li>
+                        </MOTION.li>
+                        <MOTION.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 3, delay: 2.7 }}
+                        >
                             API
-                        </li>
-                        <li>
+                        </MOTION.li>
+                        <MOTION.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 3, delay: 2.8 }}
+                        >
                             Developer
-                        </li>
+                        </MOTION.li>
                     </ul>
-                    <button>Ver Dados</button>
+                    <MOTION.button
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 2, delay: 2.9 }}
+                    >Ver Dados</MOTION.button>
                 </header>
 
                 <div className={styles.content}>
-                    <div className={styles.ref}>
+                    <MOTION.div className={styles.ref}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1, delay: 6.5 }}
+                    >
                         <span>dados de:</span> disease.sh
-                    </div>
-                    <h1>
+                    </MOTION.div>
+
+                    <MOTION.h1
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 2, delay: 4.0 }}
+                    >
                         DASHBOARD ON <br />
-                        <span>COVID-19</span>
-                    </h1>
-                    <p>Veja dados atualizados sobre COVID-19, vacinação, mortalidade, hospitais e muito mais de seu pais, ou de todo o globo.</p>
-                    <button>Ver Dashboard <FaArrowDownLong size={13}/></button>
+                        <MOTION.span
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 3, delay: 5.5 }}
+                        >COVID-19</MOTION.span>
+                    </MOTION.h1>
+
+                    <MOTION.p
+                        initial={{ opacity: 0  }}
+                        animate={{ opacity: 1}}
+                        transition={{ duration: 2, delay: 6 }}
+                    >Veja dados atualizados sobre COVID-19, vacinação, mortalidade, hospitais e muito mais de seu pais, ou de todo o globo.</MOTION.p>
+
+                    <MOTION.button
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1, delay: 6.5 }}
+                        className={styles.button}
+                    >Ver Dashboard <FaArrowDownLong size={13} /></MOTION.button>
                 </div>
             </div>
         </>
